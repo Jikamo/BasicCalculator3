6a958ec220ede43830713fa483a0cb4819c4ad1b
Jikamo <56824758+Jikamo@users.noreply.github.com>
2020-01-17 14:58:38 +0200

    Add project files.

---
+++ division.cs
@@ -0,0 +1,10 @@
+﻿namespace operations
+{
+    public static class Division
+    {
+        public static double Quotient(double a, double b)
+        {
+            return a / b;
+        }
+    }
+}
